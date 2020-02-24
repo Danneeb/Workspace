31be4e7870608ae6bb41ca6870a6bf56bd31a4a7
Daniel Bengtsson <db222pt@student.lnu.se>
2020-02-24 10:37:29 +0100

    Update PrintRecursive.java

--- 1DV507/src/db222pt_assign3/Exercise1/PrintRecursive.java
+++ 1DV507/src/db222pt_assign3/Exercise1/PrintRecursive.java
@@ -1,5 +1,17 @@
 package db222pt_assign3.Exercise1;
 
 public class PrintRecursive {
+	public static void main(String[] args) {
+		String str = "Hello Everyone!";
 
+		print(str, 0);
+		System.out.println(); // Line break
+		//printReverse(str, 0);
+	}
+	public static String print(String str, int pos) {
+		return print(str,0);
+	}
+	//public static String printReverse(String str,int pos) {
+		//return (printReverse(str,str.length()-1));
+	//}
 }
