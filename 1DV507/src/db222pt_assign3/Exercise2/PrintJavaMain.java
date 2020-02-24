8521f111b9cf581b49b9124716379a815b12d68a
Daniel Bengtsson <db222pt@student.lnu.se>
2020-02-24 11:54:06 +0100

    Update PrintJavaMain.java

--- 1DV507/src/db222pt_assign3/Exercise2/PrintJavaMain.java
+++ 1DV507/src/db222pt_assign3/Exercise2/PrintJavaMain.java
@@ -1,7 +1,12 @@
 package db222pt_assign3.Exercise2;
 
+import java.io.File;
+
 public class PrintJavaMain {
 	public static void main(String[] args) {
+		File dir = new File("C:\\Github\\repo\\Workspace\\1DV507");
+		visitSub(dir);
+		
 		
 		
 		
