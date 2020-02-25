21b26cf206d6f5181f0ee80dd301b4b9b3a5b0f7
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-02-25 10:59:32 +0100

    Update CheckXandY.java

--- Projects/src/TestProjects/CheckXandY.java
+++ Projects/src/TestProjects/CheckXandY.java
@@ -1,9 +1,20 @@
 package TestProjects;
 
 public class CheckXandY {
-
+	public static boolean compXY(String str) {
+		for(int i = 0; i<str.length();i++) {
+			if(str.charAt(i)=='x' && str.charAt(i)=='y') {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "qwasxxy";
+		
+		System.out.println(compXY(str));
 
 	}
 
