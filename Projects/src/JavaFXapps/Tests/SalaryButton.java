171b89d4a75e96a0395c77a5bcff82ecb3cfbed8
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-02-25 13:47:02 +0100

    Create SalaryButton.java

---
+++ Projects/src/JavaFXapps/Tests/SalaryButton.java
@@ -0,0 +1,22 @@
+package JavaFXapps.Tests;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SalaryButton {
+
+	public void button() {
+		Stage newWindow = new Stage();
+		newWindow.setTitle("Salary Calculator");
+
+		GridPane salaryPane = new GridPane();
+		salaryPane.setPadding(new Insets(10,10,10,10));
+		salaryPane.setVgap(5.0);
+		salaryPane.setHgap(5.0);
+		Scene sceSalary = new Scene(salaryPane);
+		newWindow.setScene(sceSalary);
+		newWindow.show();
+	}
+}
