e3142edc420eee519b1416b8bc9fc6d8ea6539ef
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-03-16 12:49:14 +0100

    Update EveryOther.java

--- Projects/src/assignment2/EveryOther.java
+++ Projects/src/assignment2/EveryOther.java
@@ -1,10 +1,20 @@
 package assignment2;
 
+import java.util.Scanner;
+
 public class EveryOther {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("First number: ");
+		int first = sc.nextInt();
+		System.out.println("Second number: ");
+		int second = sc.nextInt();
 		
-
+		for(int i = first; i<second;i=i+2) {
+			System.out.print(i+" ");
+		}
+		
 	}
 
 }
