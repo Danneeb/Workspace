d3da685b3904566520a5f4a3124121470ccf1728
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-03-17 08:34:08 +0100

    Update DangerousWork.java

--- Projects/src/assignment2/DangerousWork.java
+++ Projects/src/assignment2/DangerousWork.java
@@ -1,10 +1,24 @@
 package assignment2;
 
+import java.util.Scanner;
+
 public class DangerousWork {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double salaryDay = 0.01;
+		int day = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("how much would you like to earn? ");
+		int earn = sc.nextInt();
+		
+		while(salaryDay < earn) {
+			salaryDay = salaryDay*2;
+			day++;
+		}
+		System.out.println("You will have your money in days "+day);
+		
+		
 	}
 
 }
