ea60c4122c04cc0202399f7ad041382d165eae83
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-05-23 23:35:36 +0200

    Update NewTest.java

--- Projects/src/assignment2/NewTest.java
+++ Projects/src/assignment2/NewTest.java
@@ -1,22 +1,21 @@
 package assignment2;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NewTest {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int x = 1;
 
 		int[] arr = new int[10];
 
 
-		for(int i = 0; i<10; i++) {
-			int x = arr[i];
-			
+		for(int i = 0; i<arr.length; i++) {
+			x = x*2;
+			arr[i] = x;
 		}
-
-	
-
+		String output = Arrays.toString(arr);
+		System.out.println(output);
 	}
 }	
