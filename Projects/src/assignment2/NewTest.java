d3be8a89e084575342df2225cb9175c3f6a178e6
Daniel Bengtsson <daniel.bengtson@hotmail.com>
2020-05-23 23:26:44 +0200

    Create NewTest.java

---
+++ Projects/src/assignment2/NewTest.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+		int[] arr = new int[10];
+
+
+		for(int i = 0; i<10; i++) {
+			int x = arr[i];
+			
+		}
+
+	
+
+	}
+}	
