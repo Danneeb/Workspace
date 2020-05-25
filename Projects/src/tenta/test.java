1584633b9da1d4f3ece40d6d3475373d0ecbc3cc
Daniel Bengtsson <db222pt@student.lnu.se>
2020-05-25 23:37:52 +0200

    Create test.java

---
+++ Projects/src/tenta/test.java
@@ -0,0 +1,13 @@
+package tenta;
+
+public class test {
+
+	public static void main(String[] args) {
+		int resultat;
+		int tal = 10;
+		
+		resultat = tal-30/10 + 8*tal;
+		System.out.println(resultat);
+	}
+
+}
